7f760e2da45e286b0aafdbd2ca2f9d7de064e688
Loren Wilks <Loren.wilks@gmail.com>
2022-06-23 12:02:53 -0600

    core deliverables finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 // Your code here
+// Your code here
+const movieTitle = document. getElementById('title')
+const runTime = document.getElementById('runtime')
+const movieInfo = document.getElementById('film-info')
+const showTime = document.getElementById('showtime')
+const films = document.getElementById('films')
+const poster = document.getElementById('poster')
+const buyTickets = document.getElementById('buy-ticket')
+let tickets = document.getElementById('ticket-num')
+
+fetch('http://localhost:3000/films')
+.then(response => response.json())
+.then(data => {
+    data.forEach(renderMovieTitle)
+})
+
+buyTickets.addEventListener('click', (e) => {
+    // console.log(e)
+    if (parseInt(tickets.textContent) > 0) {
+        tickets.textContent = parseInt(tickets.textContent) - 1
+        }
+    })
+
+function renderMovieTitle(movie){
+    const newTitle = document.createElement('li')
+    newTitle.textContent = movie.title
+    films.append(newTitle)
+
+    poster.src = movie.poster
+    movieTitle.textContent = movie.title
+    runTime.textContent = `${movie.runtime} minutes`
+    movieInfo.textContent = movie.description
+    showTime.textContent = movie.showtime
+    tickets.textContent = `${movie.capacity - movie.tickets_sold}`
+    
+    newTitle.addEventListener('click', (e) => {
+        poster.src = movie.poster
+        movieTitle.textContent = movie.title
+        runTime.textContent = `${movie.runtime} minutes`
+        movieInfo.textContent = movie.description
+        showTime.textContent = movie.showtime
+        tickets.textContent = parseInt(`${movie.capacity - movie.tickets_sold}`)
+        
+    })
+    
+}
